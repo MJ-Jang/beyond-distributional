13b731915454cd7a31aa2bd92799b510ab16705c
mj <xkxpa@korea.ac.kr>
2021-07-21 21:58:27 +0900

    update train.sh

--- src/sei_experiment/train.sh
+++ src/sei_experiment/train.sh
@@ -3,9 +3,9 @@ models="electra-small electra-large bert-base bert-large roberta-base roberta-la
 for model in $models
 do
     python train.py --backbone_model_name $model --is_balanced
-    rm -rf model_binary
+    rm -rf tmp_model_binary
 
     python train.py --backbone_model_name $model --is_balanced --freeze_enc
-    rm -rf model_binary
+    rm -rf tmp_model_binary
 
 done
