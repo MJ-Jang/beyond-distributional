fcd24ca54f42d1d38d341959d867bff9d4612656
MJ-Jang <xkxpa@korea.ac.kr>
2021-10-14 12:27:53 +0100

    update model diff

---
+++ src/mm_experiment/model_diff.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import copy
+import os
+import torch
+
+from transformers import AutoModelForSequenceClassification
+from tqdm import tqdm
+
+
+pretrain_model_dict = {
+    "electra-small": ("google/electra-small-discriminator", "meaning_matching-electra-small-n_neg10"),
+    "electra-base": ("google/electra-base-discriminator", "meaning_matching-electra-base-n_neg10"),
+    "electra-large": ('google/electra-large-discriminator', "meaning_matching-electra-large-n_neg10"),
+    "bert-base": ("bert-base-cased", "meaning_matching-bert-base-n_neg10"),
+    "bert-large": ("bert-large-cased", "meaning_matching-bert-large-n_neg10"),
+    "roberta-base": ("roberta-base", "meaning_matching-roberta-base-n_neg10"),
+    "roberta-large": ("roberta-large", "meaning_matching-roberta-large-n_neg10"),
+    "albert-base": ("albert-base-v2", "meaning_matching-albert-base-n_neg10"),
+    "albert-large": ("albert-large-v2", "meaning_matching-albert-large-n_neg10")
+}
+
+
+def calculate_diff(plm1, plm2):
+    assert plm1.keys() == plm2.keys()
+    scores = []
+    for key in plm1.keys():
+
+        mat1 = plm1[key].numpy()
+        mat2 = plm2[key].numpy()
+
+        pow_ = np.power(mat1 - mat2, 2)
+        shape_ = pow_.shape
+        if len(shape_) == 1:
+            len_ = shape_
+        elif len(shape_) == 2:
+            len_ = shape_[0] * shape_[1]
+        else:
+            raise NotImplementedError
+
+        frobenius_norm = np.sqrt(np.sum(pow_))
+        avg_f_norm = frobenius_norm / len_
+        scores.append(avg_f_norm)
+    return scores
+
+
+def main():
+    outp = {
+        "model": [],
+        "mean": [],
+        "std": []
+    }
+
+    for key, (m1, m2) in tqdm(pretrain_model_dict.items(), total=len(pretrain_model_dict.keys())):
+        model1 = AutoModelForSequenceClassification.from_pretrained(m1)
+        # load model from binary file
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(dir_path, "../mm_experiment/model_binary/", f"{m2}.ckpt")
+
+        model2 = AutoModelForSequenceClassification.from_pretrained(m1)
+        model2.load_state_dict(torch.load(file_path))
+
+        if 'bert' in m1:
+            plm1 = copy.deepcopy(model1.bert.state_dict())
+            plm2 = copy.deepcopy(model2.bert.state_dict())
+        elif 'electra' in m1:
+            plm1 = copy.deepcopy(model1.bert.state_dict())
+            plm2 = copy.deepcopy(model2.bert.state_dict())
+        elif 'roberta' in m1:
+            plm1 = copy.deepcopy(model1.roberta.state_dict())
+            plm2 = copy.deepcopy(model2.roberta.state_dict())
+        elif 'albert' in m1:
+            plm1 = copy.deepcopy(model1.albert.state_dict())
+            plm2 = copy.deepcopy(model2.albert.state_dict())
+        else:
+            raise NotImplementedError
+
+        del model1, model2
+
+        scores = calculate_diff(plm1, plm2)
+        mean_, std_ = np.mean(scores), np.std(scores)
+
+        outp['model'].append(key)
+        outp['mean'].append(mean_)
+        outp['std'].append(std_)
+
+    out_df = pd.DataFrame(outp)
+    save_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../output/mm_experiment')
+    os.makedirs(save_dir, exist_ok=True)
+
+    out_df.to_csv(os.path.join(save_dir, 'model_param_diff.tsv'), sep='\t', encoding='utf-8', index=False)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
